0f0766c0b6e8ca4a5710d5a57b33fcf1301f8e06
BM <bm1120836@yandex.ru>
2022-11-06 23:16:12 +0300

    List comprehension

--- Task1.py
+++ Task1.py
@@ -1,11 +1,7 @@
 # Применение list comprehension для улучшения кода
 
+# Исходный код
 
-# Задайте список из n чисел последовательности
-# (1 + 1/n) в степени n и выведите на экран их сумму.
-# Например, для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
-
-# Первоначальный код
 print('Введите число')
 n = int(input())
 dic = {}
@@ -15,4 +11,11 @@ print(dic)
 total_sum = sum(dic.values())
 print(total_sum)
 
+
 # Применение list comprehension
+print('Введите число')
+n = int(input())
+dic = {i: (1 + 1/i)**i for i in range (1, n+1)}
+print(dic)
+total_sum = sum(dic.values())
+print(total_sum)
